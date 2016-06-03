438d41a2bb298cf286b606d73a96095e2db44862
Elizabeth Harper <eharper@excelmicro.com>
2016-06-03 16:42:05 -0400

    Introduced some minor tests and corrected issues based on them

--- test/io/vulpine/commons/NullUtilsTest.java
+++ test/io/vulpine/commons/NullUtilsTest.java
@@ -1,5 +1,50 @@
 package io.vulpine.commons;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
 public class NullUtilsTest
 {
+  @Test
+  public void testCoalesceReturnsNull()
+  {
+    assertNull(
+      "NullUtils.coalesce() should return null when no non-null value is provided.",
+      NullUtils.coalesce(null, null, null, null, null)
+    );
+  }
+
+  @Test
+  public void testCoalesceReturnsNotNull()
+  {
+    assertNotNull(
+      "NullUtils.coalesce() should not return null if a non-null value is provided.",
+      NullUtils.coalesce(null, null, null, null, "Test")
+    );
+  }
+
+  @Test
+  public void testCoalesceReturnsFirstNonNull()
+  {
+    final Map < String, String > realValue = new HashMap <>();
+    final Map < String, String > fakeValue = new HashMap <>();
+
+    realValue.put("This is", "a test map");
+    realValue.put("that should", "be the one returned from coalesce.");
+
+    fakeValue.put("This is", "a test map");
+    fakeValue.put("that shouldn't", "be returned from coalesce.");
+
+    assertEquals(
+      "NullUtils.coalesce() should return the first non-null value provided to it.",
+      NullUtils.coalesce(null, null, null, realValue, fakeValue),
+      realValue
+    );
+  }
 }
--- test/io/vulpine/commons/StringUtilTest.java
+++ test/io/vulpine/commons/StringUtilTest.java
@@ -1,5 +1,108 @@
 package io.vulpine.commons;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 public class StringUtilTest
 {
+  public static class Method_nullToEmpty {
+
+    @Test
+    public void When_Param1IsNull_ReturnEmptyString ()
+    {
+      assertEquals(
+        "StringUtils.nullToEmpty() should return an empty string when passed a null value.",
+        "",
+        StringUtils.nullToEmpty(null)
+      );
+    }
+
+    @Test
+    public void When_Param1IsNotNull_ReturnParam1 ()
+    {
+      final String testString = "This is a test string.";
+
+      assertEquals(
+        "StringUtils.nullToEmpty() should return the value passed to it if that value is not null.",
+        testString,
+        StringUtils.nullToEmpty(testString)
+      );
+    }
+  }
+
+  public static class Method_implode {
+
+    @Test
+    public void When_Param1IsStringAndParam2IsStringArray_ReturnConcatenatedArrayValues ()
+    {
+      final String[] testArray  = new String[] { "This", "is", "the", "expected", "output", "value" };
+      final String   glueString = "-_-";
+      final String testString = testArray[0] + glueString + testArray[1] + glueString + testArray[2] + glueString
+        + testArray[3] + glueString + testArray[4] + glueString + testArray[5];
+
+      assertEquals(
+        "StringUtils.implode() should join a string using the specified 'glue' string.",
+        testString,
+        StringUtils.implode(glueString, testArray)
+      );
+    }
+
+    @Test
+    public void When_Param1IsNull_ReturnNull()
+    {
+      assertNull(
+        "StringUtils.implode() should return null if passed null values to prevent internal errors.",
+        StringUtils.implode(null, new String[] {"Test", "Values"})
+      );
+    }
+
+    @Test
+    public void When_Param2IsNull_ReturnNull()
+    {
+      assertNull(
+        "StringUtils.implode() should return null if passed null values to prevent internal errors.",
+        StringUtils.implode(" ", null)
+      );
+    }
+
+    @Test
+    public void When_Param2IsEmptyArray_ReturnEmptyString()
+    {
+      assertEquals(
+        "StringUtils.implode() should return an empty string when passed an empty array of string 'pieces'.",
+        "",
+        StringUtils.implode(" ", new String[]{})
+      );
+    }
+  }
+
+
+
+  @Test
+  public void wrapShouldWrapStringsToAGivenLengthOnSpacesOnly()
+  {
+    final String testInput = "I am a string that is 82 characters long and I should be wrapped at 20 characters.";
+    final String expected = "I am a string that\nis 82 characters\nlong and I should be\nwrapped at 20\ncharacters.";
+
+    assertEquals(
+      "StringUtils.wrap() should wrap strings to a given length on the last space previous to hitting the length limit.",
+      expected,
+      StringUtils.wrap(testInput, 20)
+    );
+  }
+
+  @Test
+  public void wrapToArrayShouldWrapStringsToAnArrayOfSubStringsOfAGivenLengthOnSpacesOnly()
+  {
+    final String testInput = "I am a string that is 82 characters long and I should be wrapped at 20 characters.";
+    final String expected = "I am a string that\nis 82 characters\nlong and I should be\nwrapped at 20\ncharacters.";
+
+    assertEquals(
+      "StringUtils.wrap() should wrap strings to a given length on the last space previous to hitting the length limit.",
+      expected,
+      StringUtils.wrap(testInput, 20)
+    );
+  }
 }
